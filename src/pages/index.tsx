2691d22781643a0b6be2f3aa16cbea0ac0cb4b61
joemama0s <tylerquast68@gmail.com>
2023-04-04 00:04:20 -0700

    pushing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div>
-      <h1> this is a test with a new push main again</h1>
+      <h1> this is a test with a new push main again some change</h1>
     </div>
   );
 }
